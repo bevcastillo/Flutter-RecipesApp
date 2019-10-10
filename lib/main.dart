8c30adbdd0b7bc6396ac33ce028b5557244eea74
Beverly May Sellon Castillo <beverlymaycastillo@gmail.com>
2019-10-10 16:08:08 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:recipes_app/app.dart';
+
+void main() => runApp(
+    new RecipesApp(),
+);
---
+++ lib/ui/screens/login.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:recipes_app/ui/widgets/google_sign_in_button.dart';
+
+class LoginScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    BoxDecoration _buildBackground() {
+      return BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/login_backdrop.jpeg"),
+            fit: BoxFit.cover,
+        ),
+      );
+    }
+
+
+      Text _buildText() {
+        return Text(
+            'Recipes',
+            style: Theme.of(context).textTheme.headline,
+            textAlign: TextAlign.center,
+        );
+      }
+
+      return Scaffold(
+        body: Container(
+          decoration: _buildBackground(),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _buildText(),
+                SizedBox(height: 50.0,),
+                GoogleSignInButton(
+                  onPressed: () => print("Button pressed"),
+                )
+              ],
+            ),
+            ),
+        ),
+      );
+  }
+}
